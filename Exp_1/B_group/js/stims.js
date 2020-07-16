268d1431a31cd83fff94d5f3160e4812e5094c5c
Will Clapp <wsclapp@stanford.edu>
2020-07-16 15:32:21 -0500

    restructured; added some json stuff

---
+++ Exp_1/B_group/js/stims.js
@@ -0,0 +1,53 @@
+
+var word_trial_stimuli = [
+  {
+    "index": "1",
+    "set": "Ballerina_Pallerina_1",
+    "voiced": "B",
+    "voiceless": "P",
+    "step": "12",
+    "underscore": "_",
+    "extension": ".mp3",
+    "path": "Ballerina_Pallerina_1_15.mp3"
+  },
+  {
+    "index": "2",
+    "set": "Memory_1",
+    "voiced": "B",
+    "voiceless": "P",
+    "step": "13",
+    "underscore": "_",
+    "extension": ".mp3",
+    "path": "Memory_1.mp3"
+  }
+]
+
+var nonword_trial_stimuli = [
+  {
+    "index": "1",
+    "set": "BAH_PAH_3",
+    "voiced": "B",
+    "voiceless": "P",
+    "step": "23",
+    "underscore": "_",
+    "extension": ".mp3",
+    "path": "BAH_PAH_3_0.mp3"
+  },
+  {
+    "index": "2",
+    "set": "DIH_TIH_3_22.mp3",
+    "voiced": "B",
+    "voiceless": "P",
+    "step": "20",
+    "underscore": "_",
+    "extension": ".mp3",
+    "path": "DIH_TIH_3_22.mp3.mp3"
+  }
+]
+
+var assetPaths = [
+  "audio/exposure_B_group/Ballerina_Pallerina_1_15.mp3",
+  "audio/exposure_B_group/Memory_1.mp3",
+  "audio/experimental/Barn_Parn_1_12.mp3",
+  "audio/experimental/Bawn_Pawn_3_21.mp3"
+]
---
+++ Exp_1/import_csv_B_group.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const csv = require('csv-parser')
+let exposureResults = []
+let testResults = []
+let assetPaths = []
+
+const buildOutput = (word, nonword, assets) => `
+var exposure = ${JSON.stringify(word, null, 2)}
+
+var test = ${JSON.stringify(nonword, null, 2)}
+
+var assetPaths = ${JSON.stringify(assets.flat(), null, 2)}`
+
+
+
+
+// This is which stims.js file is getting written
+const writeResults = (word, nonword, assets) => {
+	fs.writeFile('B_group/js/stims2.js', buildOutput(word, nonword, assets), err => {
+		if (err) {
+			console.error(err)
+		}
+		console.log("stims.js written")
+	})
+}
+
+// Getting read for trial
+fs.createReadStream('B_group/trial_csv/exposure_B_group.csv')
+	.pipe(csv())
+	.on('data', data => {
+		exposureResults.push(data)
+		// This is just for preloading!
+		assetPaths.push([
+			'audio/exposure_B_group/' + data.path, 
+		])
+	})
+	.on('end', () => {
+		console.log('done reading exposure_word.csv')
+		fs.createReadStream('B_group/trial_csv/test_B_group.csv')
+			.pipe(csv())
+			.on('data', data => {
+				experimentalResults.push(data)
+				assetPaths.push([
+					'audio/experimental/' + data.path, 
+				])
+			})
+			.on('end', () => {
+				console.log('done reading exposure_nonword.csv')
+				writeResults(wordResults, nonwordResults, assetPaths)
+			})
+	})
+
+
+
+
+
+
+
