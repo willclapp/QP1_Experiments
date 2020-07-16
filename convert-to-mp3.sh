1c8fbf6043e672c2ffb9559ea39434968194696d
Will Clapp <wsclapp@stanford.edu>
2020-07-16 00:09:11 -0500

    Finalized stims and converted to mp3. Updated trial csvs

---
+++ convert-to-mp3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+arr=("./Exp_1/audio/experimental/*" "./Exp_1/audio/exposure_B_group/*" "./Exp_1/audio/exposure_P_group/*" "./Exp_1/audio/exposure_control/*")
+
+# arr=("experiment_no_shift/audio/context/*")
+
+for d in "${arr[@]}"; do
+	echo "converting audio in ${d}"
+
+	for i in $d; do
+		sox "$i" "${i%.wav}.mp3"
+		echo "created ${i%.wav}.mp3"
+		rm $i
+	done
+	# rm "${d}.wav"
+	# echo "removed ${d}.wav"
+done
